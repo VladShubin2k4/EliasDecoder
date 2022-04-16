d7db60b452d2e738cd15d2a798ccd46b0dd0a504
Vlad <VladShubin2k4@outlook.com>
2022-04-16 18:59:55 +0300

    Moved code from an old and incorrect repository and fixed one vulnerability

--- EliasDecoder.cpp
+++ EliasDecoder.cpp
@@ -1,13 +1,12 @@
 ﻿#include <vector>
 #include <string>
-//#include <cstdlib>
 #include <iostream>
 #include <windows.h>
 using namespace std;
 
-int transfer(int byte, short radix) {
+int transfer(int byte, int radix) {
     int res = 0, k = 1;
-    for (short i = 0; byte != 0; ++i) {
+    for (int i = 0; byte != 0; ++i) {
         res += (byte % radix) * k;
         k <<= 1;
         byte /= radix;
@@ -15,8 +14,8 @@ int transfer(int byte, short radix) {
     return res;
 }
 
-void Decompress(vector<string>& dec, short& arg) {
-    for (short i = dec.size() - 1; i >= 0; --i) {
+void Decompress(vector<string>& dec, int arg) {
+    for (int i = dec.size() - 1; i >= 0; --i) {
         arg %= 2;
         int cnt = stoi(dec[i]);
         dec[i].clear();
@@ -25,10 +24,9 @@ void Decompress(vector<string>& dec, short& arg) {
     }
 }
 
-void DeleteNull(string& str, bool& first) {
-    short cnt = 0;
-    if (!first) --cnt;
-    for (short i = 0; i < str.size() && str[i] != '\0'; ++i) {
+void DeleteNull(string& str) {
+    int cnt = 0;
+    for (int i = 0; i < str.size() && str[i] != '\0'; ++i) {
         if (str[i] == '0') {
             ++cnt;
             str.erase(i--, 1);
@@ -42,14 +40,14 @@ void DeleteNull(string& str, bool& first) {
     }
 }
 
-vector<string> InDEC(string& str) {
+vector<string> InDEC(const string& str) {
     vector<string> res;
     int combo, p;
-    for (short i = str.size() - 1; i >= 0; --i) {
+    for (int i = str.size() - 1; i >= 0; --i) {
         if (str[i] == ' ') {
             combo = 0;
             p = 0;
-            for (short j = i - 1; str[j] != ' ' && j >= 0; --j)
+            for (int j = i - 1; j >= 0 && str[j] != ' ' ; --j)
                 combo = combo + (static_cast<int>(str[j]) - 48) * static_cast<int>(1 << p++);
             res.push_back(to_string(combo));
         }
@@ -57,18 +55,18 @@ vector<string> InDEC(string& str) {
     return res;
 }
 
-void Decode_and_Otput(vector<string>& dec) {
+void Decode_and_Otput(const vector<string>& dec) {
     string res, symb;
-    for (short i = 0; i < dec.size(); ++i) res += dec[i];
-    short isfull = (res.size()) % 8;
+    for (int i = 0; i < dec.size(); ++i) res += dec[i];
+    int isfull = (res.size()) % 8;
     if (isfull) {
-        short cntnon = isfull;
-        for (short j = res.size() - 1; --cntnon >= 0; --j)  symb += res[j];
+        int cntnon = isfull;
+        for (int j = res.size() - 1; --cntnon >= 0; --j)  symb += res[j];
         unsigned char symbol = transfer(stoi(symb), 10);
         symb.clear();
         cout << symbol;
     }
-    for (short j = res.size() - isfull - 1; j >= 0; --j) {
+    for (int j = res.size() - isfull - 1; j >= 0; --j) {
         symb += res[j];
         if (symb.size() == 8) {
             unsigned char symbol = static_cast<unsigned char>(transfer(stoi(symb), 10));
@@ -76,26 +74,25 @@ void Decode_and_Otput(vector<string>& dec) {
             cout << symbol;
         }
     }
+    cout << endl;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     SetConsoleCP(1251); SetConsoleOutputCP(1251);
-    //freopen("res.txt","w",stdout);
-    bool drop1st = false;
     string str;
     getline(cin, str);
 
-    if (str[0] != 48) drop1st = true;
-    for (short i = 0; i < str.size(); ++i) if (str[i] == ' ') str.erase(i, 1);
-    DeleteNull(str, drop1st);
+    str.erase(0, 1);
+    for (int i = 0; i < str.size(); ++i) if (str[i] == ' ') str.erase(i, 1);
+
+    DeleteNull(str);
     vector <string> dec = InDEC(str);
 
-    if (drop1st) dec.pop_back();
-    short arg = static_cast<short>(drop1st);
-
-    Decompress(dec, arg);
+    Decompress(dec, str[0] % 48);
     Decode_and_Otput(dec);
+    
+    system("pause");
     return 0;
 }
 
